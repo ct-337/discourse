8c12f7783b102c82e27408b8310d756c05288cdc
ct-337 <themathematicalcomet@gmail.com>
2025-10-20 20:34:29 -0400

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -4,9 +4,11 @@
 gem install bundler
 
 # Install Ruby gems
+bundle config set deployment 'true'
+bundle config set without 'development test'
 bundle install --jobs=4 --retry=3
 
-# Precompile assets (optional but recommended)
+# Precompile assets (Discourse needs this)
 bundle exec rake assets:precompile
 
 # Exit cleanly
